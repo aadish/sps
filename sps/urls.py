b0f7926520574cf6150165c8f1a256fc1222d368
Aadish Gupta <aadish@digitalgreen.org>
2014-11-30 16:00:52 +0530

    dashboard.html

--- sps/urls.py
+++ sps/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,4 +14,5 @@ urlpatterns = patterns('',
 )
 
 if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+    #urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
